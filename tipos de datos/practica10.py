9f4dd568f6d880a7acafc68f2cbe7100905f00ee
Raulmfdz <raumfdz@gmail.com>
2026-09-24 18:55:27 +0200

    Add files via upload

---
+++ tipos de datos/practica10.py
@@ -0,0 +1,5 @@
+payaso = 112
+muñeca = 75
+cantidad_payasos = int(input("Ingrese la cantidad de payasos: "))
+cantidad_muñecas = int(input("Ingrese la cantidad de muñecas: "))
+print(f"El peso total del paquete es de: { (payaso * cantidad_payasos) + (muñeca * cantidad_muñecas) } gramos")
