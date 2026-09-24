1b69e7a81a96c6408879e0138f5932cfc01477d2
RaulMF <raumfdz@gmail.com>
2026-09-24 14:23:40 +0200

    practica5

--- tipos de datos/practica5.py
+++ tipos de datos/practica5.py
@@ -1,3 +1,3 @@
 numeroh = int(input("Ingrese un número de hors que trabajas: "))
-costo_h = int(input("Ingrese el costo por hora: ")) 
+costo_h = float(input("Ingrese el costo por hora: ")) 
 print(f"El total a pagar es: {numeroh * costo_h}$")
