551d9ac2778a8bc19900eb7cfc6657e91d57e7d7
RaulMF <raumfdz@gmail.com>
2026-09-24 14:39:58 +0200

    practica8

---
+++ tipos de datos/practica8.py
@@ -0,0 +1,6 @@
+numero1 = int(input("Ingrese el primer número entero: "))
+numero2 = int(input("Ingrese el segundo número entero: "))
+c = numero1 // numero2
+r = numero1 % numero2
+print(f"El cociente de {numero1} entre {numero2} es: {c}")
+print(f"El resto de {numero1} entre {numero2} es: {r}")
